affb3bf06a43529250cd3f47ed4c9f12a6a0e885
Jonathan Colmenares <jonathan@yolouso.com>
2017-12-16 20:30:42 -0500

    Change modid

--- src/main/java/com/theishiopian/beacon/Core.java
+++ src/main/java/com/theishiopian/beacon/Core.java
@@ -9,13 +9,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Core.MODID, version = Core.VERSION)
 public class Core
 {
-    public static final String MODID = "beacon";
+    public static final String MODID = "lloydbeacon";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void preinit(FMLPreInitializationEvent event)
     {
         ModItems.init();
-    	
     }
 }
